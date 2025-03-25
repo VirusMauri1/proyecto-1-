4f0650444267848452a8a359e1defc2a65f5440f
Deniss3232 <iqdenis@gmail.com>
2025-03-25 00:34:48 -0600

    Update Environment.java

--- Environment.java
+++ Environment.java
@@ -12,18 +12,3 @@ public class Environment {
     public Environment(Environment parent) {
         this.parent = parent;
     }
-
-    public void define(String name, Object value) {
-        variables.put(name, value);
-    }
-
-    public Object lookup(String name) {
-        if (variables.containsKey(name)) {
-            return variables.get(name);
-        } else if (parent != null) {
-            return parent.lookup(name);
-        } else {
-            throw new RuntimeException("Variable no definida: " + name);
-        }
-    }
-}
